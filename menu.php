532813133f9fa3f52743da2930fd0d794a40fe75
fatima-dachari <83180703+fatima-dachari@users.noreply.github.com>
2021-04-25 15:33:19 -0300

    commit inicial

---
+++ menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+</head>
+<body>
+    <div class="menu">
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Quem somos</a></li>
+                <li><a href="">Produtos</a>
+                    <ul>
+                        <li><a href="">Produto A</a></li>
+                        <li><a href="">Produto B</a></li>
+                        <li><a href="">Produto C</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Contato</a></li>
+                <li><a href="">Entrar</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
